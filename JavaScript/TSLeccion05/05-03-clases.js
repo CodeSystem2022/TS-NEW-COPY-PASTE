2d643629799e38949c1a24bbd6a25e7a91be863a
sverguecio <66077784+sverguecio@users.noreply.github.com>
2023-05-29 20:13:20 -0300

    Add files via upload

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -1,6 +1,7 @@
 
-
-class Persona{//clase padre
+//let persona3 = new Persona("carla", "ponce"); //esto no se puede hacer: persona is not defined
+//clase padre
+class Persona{
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido =apellido;
@@ -22,6 +23,16 @@ class Persona{//clase padre
         this._apellido=apellido;
     }
 
+    nombreCompleto(){
+        return this._nombre+" "+this._apellido;
+    }
+    //sobreescribiendo el metodo de la clase padre(object)
+    toString(){//regresa un string
+       // se aplica el polimorfismo que significa = multiples formas en tiempo de ejecucion
+       //el metodo que se ajecuta depende si es una referencia de tipo padre o hija
+        return this.nombreCompleto();
+    }
+
 }
 class Empleado extends Persona{//CLASE HIJA
     constructor(nombre, apellido, departamento){
@@ -35,6 +46,11 @@ class Empleado extends Persona{//CLASE HIJA
     }
     set departamento(departamento){
         this._departamento=departamento;
+    }
+    //sobreescritura estamos modificando el metodo con el mismo nombre
+
+    nombreCompleto(){
+        return super.nombreCompleto()+", "+this._departamento;
     }
 }
 
@@ -68,5 +84,17 @@ console.log(persona2.apellido)
 
 let empleado1 = new Empleado("maria", "gimenez", "sistemas");
 console.log(empleado1);
-console.log(empleado1.nombre)
+console.log(empleado1.nombreCompleto());
+
+//Object.prototype.toString manera de acceder a atributos de manera dinamica
+
+
+console.log(empleado1.toString());
+
+console.log(persona1.toString());
+
+
+
+
+
 
