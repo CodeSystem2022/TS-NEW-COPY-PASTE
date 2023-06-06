6c2cd5f857c517ed821bd51fb09be2c0ccbf7647
matias gimenez <106162059+matucamus@users.noreply.github.com>
2023-06-06 13:26:26 -0300

    clase 7
    Clase 7 Interfaces en Java

---
+++ JAVA/Leccion07/JavaBeans/src/test/TestJavaBeans.java
@@ -0,0 +1,18 @@
+
+package test;
+
+import domain.Persona;
+
+public class TestJavaBeans {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("JUAN");
+        persona.setApellido("PEREZ");
+        System.out.println("persona = " + persona);
+        
+        System.out.println("Persona nombre: "+persona.getNombre());
+        System.out.println("Persona apellido: "+persona.getApellido() );
+
+    }
+    
+}
